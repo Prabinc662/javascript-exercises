8d2d3a4bf7b63064f76081f3c1053f09dfd22f98
Prabin Chaudhary <prabinc494@gmail.com>
2025-04-28 18:02:48 +0100

    complete Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (parseInt(num1) >= 0 && parseInt(num2) >= 0 && Number.isInteger(num1) &&
+ Number.isInteger(num2)) {
+        if (num1 < num2) {
+            for (let i = parseInt(num1); i <= parseInt(num2); i++) {
+                sum = sum + i;
+            } 
+        } else if (num1 > num2) {
+            for (let j = parseInt(num2); j <= parseInt(num1); j++) {
+                sum = sum + j;
+            }
+        }
+    } else {
+        return "ERROR";
+    }
 
+    return sum;
 };
 
 // Do not edit below this line
