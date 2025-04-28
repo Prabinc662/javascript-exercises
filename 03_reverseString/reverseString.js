0258d3a1a4fe8d47ed03bd0ce7dab928132f0b74
Prabin Chaudhary <prabinc494@gmail.com>
2025-04-28 10:45:31 +0100

    complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(words) {
+    let word = '';
+    for (i = words.length - 1; i >=0; i--) {
+        word = word + words[i];
+    }
+    return word;
 };
+
+console.log(reverseString('hello'));
 
 // Do not edit below this line
 module.exports = reverseString;
