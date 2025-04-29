53926db4a506947fc142d0a8cf035462fed05a3c
Prabin Chaudhary <prabinc494@gmail.com>
2025-04-29 17:34:33 +0100

    complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  let celsius = (fahr - 32) / 1.8;
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let fahr = cel * 1.8 + 32;
+  return Math.round(fahr * 10) / 10;
 };
 
 // Do not edit below this line
